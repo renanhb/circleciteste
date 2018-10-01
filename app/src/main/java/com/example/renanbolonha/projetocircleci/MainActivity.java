dd95e1665322bdb3360bee191b367c84070eeff8
Renan Bolonha <renan.bolonha@hbsis.com.br>
2018-10-01 11:44:17 -0300

    - Corrigido Bug;

--- app/src/main/java/com/example/renanbolonha/projetocircleci/MainActivity.java
+++ app/src/main/java/com/example/renanbolonha/projetocircleci/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: 06/08/2018 - AAAAAAAAAA 
         // TODO: 06/08/2018 - BBBBBBBBBB aasda
+        corrigidoBug1();
+    }
+
+    private void corrigidoBug1() {
+        Toast.makeText(getApplicationContext(), "BUG REMOVIDO", Toast.LENGTH_SHORT).show();
 
     }
 }
